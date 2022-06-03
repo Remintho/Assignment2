a6a355e8f87fbfbf36c99937ff32cdde1659c23d
remindme00 <84369665+remindme00@users.noreply.github.com>
2022-06-03 14:52:36 +1200

    version 13

--- src/GUI/AdminPage.java
+++ src/GUI/AdminPage.java
@@ -651,10 +651,11 @@ public class AdminPage extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(reportTabbed, javax.swing.GroupLayout.Alignment.TRAILING)
+            .addComponent(reportTabbed, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 609, Short.MAX_VALUE)
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void addUserButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addUserButtonActionPerformed
